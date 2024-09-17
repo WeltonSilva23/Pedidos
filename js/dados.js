bcd9afeb167ab765e0dbe713eb3184911376d11e
Welton Silva <131977457+WeltonSilva23@users.noreply.github.com>
2024-09-17 11:10:39 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,100 @@
+var MENU = {
+    "burgers": [
+        {
+            "id": "banana-nanica",
+            "img": "./Produto/Banana-Nanica.jpg",
+            "name": "Banana Nanica",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 10
+        },
+        {
+            "id": "banana-prata",
+            "img": "./Produto/Banana-Prata.jpg",
+            "name": "Banana Prata",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 10
+        },
+        {
+            "id": "banana-maca",
+            "img": "./Produto/Banana-Maçã.jpg",
+            "name": "Banana Maça",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 10
+        },
+        {
+            "id": "maca-gala",
+            "img": "./Produto/Maça-Gala.jpg",
+            "name": "Maçã Gala",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 109
+        },
+        {
+            "id": "maca-fuji",
+            "img": "./Produto/maca-Fuji.jpg",
+            "name": "Maçã Fuji",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 109
+        },
+        {
+            "id": "maca-argentina",
+            "img": "./Produto/Maça-Argentina.jpeg",
+            "name": "Maçã Argentina",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 109
+        },
+        {
+            "id": "pera-portuguesa",
+            "img": "./Produto/Pera-Portuguesa.jpeg",
+            "name": "Perâ Portuguesa",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 109
+        },
+        {
+            "id": "abacate",
+            "img": "./Produto/Abacate.jpeg",
+            "name": "Abacate",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 109
+        },
+        
+    ],
+    
+    "pizzas": [
+        {
+            "id": "cenoura",
+            "img": "./Produto/Cenoura.jpg",
+            "name": "Cenoura",
+            "dsc": "Terra Vegetal 500g",
+            "price": 67.99
+        },
+        {
+            "id": "batata-monalisa",
+            "img": "./Produto/Batata-Monalisa.jpg",
+            "name": "Batata Monalisa",
+            "dsc": "Terra Vegetal 1kg",
+            "price": 139
+        },
+        {
+            "id": "pimentao-verde",
+            "img": "./Produto/Pimentao-Verde.jpg",
+            "name": "Pimentão Verde",
+            "dsc": "Terra Vegetal 2kg",
+            "price": 79
+        },
+        {
+            "id": "tomate-extra",
+            "img": "./Produto/Tomate-Extra.jpeg",
+            "name": "Tomate Extra",
+            "dsc": "Terra Vegetal 5kg",
+            "price": 116.99
+        },
+        {
+            "id": "vagem",
+            "img": "./Produto/Vagem.jpg",
+            "name": "Vagem",
+            "dsc": "Terra Vegetal 5kg",
+            "price": 116.99
+        },
+    ],
+
+}
